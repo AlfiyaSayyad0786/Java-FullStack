5c960489aad4bcce154c331f48140dc44ee043af
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 02:00:14 -0700

    Added data types double in program

---
+++ 01-Basics/Data Types/Double/doubleexample.java
@@ -0,0 +1,9 @@
+package Double;
+
+public class doubleexample {
+    public static void main(String[] args) {
+        double percentage = 95.5;
+        System.out.println(percentage);
+    }
+    
+}
