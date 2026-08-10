bfafd6fd3edb097f941e6cda7f1e5787947c24eb
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 02:02:58 -0700

    Added data types program in boolean example

---
+++ 01-Basics/Data Types/Boolean/booleanexample.java
@@ -0,0 +1,10 @@
+package Boolean;
+
+public class booleanexample {
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        System.out.println(isStudent);
+
+    }
+    
+}
