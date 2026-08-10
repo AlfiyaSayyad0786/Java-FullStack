a1f3717e5e01a1b7ed0d8488bb7046e55ad24b64
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 00:00:53 -0700

    Added boolea example in program

---
+++ 01-Basics/Variables/Boolean/Booleanexample.java
@@ -0,0 +1,9 @@
+package Boolean;
+
+public class Booleanexample {
+    public static void main(String[] args) {
+        boolean isJavaEasy = true;
+        System.out.println(isJavaEasy);
+    }
+    
+}
