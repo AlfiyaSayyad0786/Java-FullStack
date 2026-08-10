68f11513ae4d478b95db8c8ba20ad486fc3768c0
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 01:53:49 -0700

    Added data types in program in example

--- 01-Basics/Data Types/Boolean/booleanexample.java
+++
@@ -1,9 +0,0 @@
-package Boolean;
-
-public class booleanexample {
-    public static void main(String[] args) {
-        boolean isStudent = true;
-        System.out.println(isStudent);
-    }
-    
-}
---
+++ 01-Basics/Data Types/Long/longexample.java
@@ -0,0 +1,9 @@
+package Long;
+
+public class longexample {
+    public static void main(String[] args) {
+        long population = 1400000L;
+        System.out.println(population);
+    }
+    
+}
