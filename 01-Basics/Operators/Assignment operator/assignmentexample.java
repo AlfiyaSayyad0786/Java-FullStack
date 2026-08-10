3b83c34e2f1eb262e37225aeaf92ad1d469fd3a3
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 06:15:59 -0700

    Added operator example in assigment operator program

---
+++ 01-Basics/Operators/Assignment operator/assignmentexample.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+public class AssignmentExample {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println("Initial value = " + a);
+
+        a += 5;
+        System.out.println("After += " + a);
+
+        a -= 3;
+        System.out.println("After -= " + a);
+
+        a *= 2;
+        System.out.println("After *= " + a);
+
+        a /= 4;
+        System.out.println("After /= " + a);
+
+        a %= 3;
+        System.out.println("After %= " + a);
+    }
+}
