ebd91db7691e2e99e8da0931ce285647b3f4ccfd
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 04:45:55 -0700

    Added type conversion in narrowing

---
+++ 01-Basics/Type Conversion/Narrowing.java/Main.java
@@ -0,0 +1,14 @@
+Package Narrowing {
+    public class Main {
+    public static void main(String[] args) {
+        double price = 99.99;
+
+        int result = (int) price;
+
+        System.out.println("Double = " + price);
+        System.out.println("Integer = " + result);
+    }
+}
+}
+    
+
