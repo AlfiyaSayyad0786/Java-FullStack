167ad8d55b2d8e863d72b0f67021fb5de5c76a4c
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 06:30:11 -0700

    Added operator example relational operator in program

---
+++ 01-Basics/Operators/Logical operator/logicalexample.java
@@ -0,0 +1,8 @@
+package Logical operator;
+
+public class logicalexample {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ 01-Basics/Operators/Relational operator/relationalexample.java
@@ -0,0 +1,16 @@
+package Relational;
+
+public class RelationalExample {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("a >= b = " + (a >= b));
+        System.out.println("a <= b = " + (a <= b));
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+    }
+}
