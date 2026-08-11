ffa573b77bf336a1835297b5edec0116293b05ce
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 20:28:50 -0700

    Added in if else if program

---
+++ 01-Basics/Conditional Statements/if else if ladder/if else if.java
@@ -0,0 +1,22 @@
+package if else if ladder;
+
+public class if else if {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 75) {
+            System.out.println("Grade B");
+        } else if (marks >= 60) {
+            System.out.println("Grade C");
+        } else if (marks >= 35) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
+    
+}
