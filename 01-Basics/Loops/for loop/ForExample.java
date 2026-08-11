ec15eab4fc988b3556250747bdba081582b7a1d5
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 20:46:12 -0700

    Added for loops program

---
+++ 01-Basics/Loops/for loop/ForExample.java
@@ -0,0 +1,10 @@
+package Loops;
+public class ForExample {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
