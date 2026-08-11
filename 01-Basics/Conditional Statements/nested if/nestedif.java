6f5afbd38248a97d88159be29e6de3dc43d5a6e7
AlfiyaSayyad0786 <sayyadalfiya@00gmail.com>
2026-08-10 20:35:25 -0700

    Added nested if program

---
+++ 01-Basics/Conditional Statements/nested if/nestedif.java
@@ -0,0 +1,16 @@
+package nested if;
+
+public class nestedif {
+     public static void main(String[] args) {
+
+        int age = 20;
+        boolean hasId = true;
+
+        if (age >= 18) {
+            if (hasId) {
+                System.out.println("Entry Allowed");
+            }
+        }
+    }
+}
+    
